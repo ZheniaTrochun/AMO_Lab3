836b0689d35dd04a040a925167e2b20ccfb733af
ZheniaTrochun <zheniatrochun@ukr.net>
2017-04-13 01:00:29 +0300

    bugs fixed

--- viev/Template/js/main.js
+++ viev/Template/js/main.js
@@ -1,11 +1,11 @@
 
-// const Chart = require('Chart');
-
 let interRes = [];
 let arr = [];
 
 
 const sinclick = () => {
+  if(!validation()) return;
+
   $.ajax({
     type: 'POST',
     url: 'http://localhost:8080/interpolate/sin',
@@ -16,42 +16,11 @@ const sinclick = () => {
     },
 
     success: (res) => {
-    //  console.log(res);
 
       arr = res[0];
       interRes[0] = res[1];
 
       drawChart('interpolated', 'int-parent', arr, res[1], 'interpolated sin(x)');
-
-      // let ctx = document.getElementById('interpolated');
-      // let interpolatedChart = new Chart(ctx, {
-      //   type: 'line',
-      //   data: {
-      //     labels: res[0],
-      //     datasets: [{
-      //       fill: false,
-      //       lineTension: 0.1,
-      //       backgroundColor: "rgba(75,192,192,0.4)",
-      //       borderColor: "rgba(75,192,192,1)",
-      //       borderCapStyle: 'butt',
-      //       borderDash: [],
-      //       borderDashOffset: 0.0,
-      //       borderJoinStyle: 'miter',
-      //       pointBorderColor: "rgba(75,192,192,1)",
-      //       pointBackgroundColor: "#fff",
-      //       pointBorderWidth: 1,
-      //       pointHoverRadius: 5,
-      //       pointHoverBackgroundColor: "rgba(75,192,192,1)",
-      //       pointHoverBorderColor: "rgba(220,220,220,1)",
-      //       pointHoverBorderWidth: 2,
-      //       pointRadius: 1,
-      //       pointHitRadius: 10,
-      //       label: "interpolated sin(x)",
-      //       data: res[1]
-      //     }]
-      //   }
-      //
-      // });
 
       $('#interpolated').show();
 
@@ -68,47 +37,17 @@ const sinclick = () => {
     },
 
     success: (res) => {
-      //console.log(res);
 
       interRes[1] = res;
 
       drawChart('func', 'func-parent', arr, res, 'sin(x)');
-      //
-      // let ctx1 = ;
-      // let funcChart = new Chart(document.getElementById('func'), {
-      //   type: 'line',
-      //   data: {
-      //     labels: arr,
-      //     datasets: [{
-      //       fill: false,
-      //       lineTension: 0.1,
-      //       backgroundColor: "rgba(75,192,192,0.4)",
-      //       borderColor: "rgba(75,192,192,1)",
-      //       borderCapStyle: 'butt',
-      //       borderDash: [],
-      //       borderDashOffset: 0.0,
-      //       borderJoinStyle: 'miter',
-      //       pointBorderColor: "rgba(75,192,192,1)",
-      //       pointBackgroundColor: "#fff",
-      //       pointBorderWidth: 1,
-      //       pointHoverRadius: 5,
-      //       pointHoverBackgroundColor: "rgba(75,192,192,1)",
-      //       pointHoverBorderColor: "rgba(220,220,220,1)",
-      //       pointHoverBorderWidth: 2,
-      //       pointRadius: 1,
-      //       pointHitRadius: 10,
-      //       label: "sin(x)",
-      //       data: res
-      //     }]
-      //   }
-      // });
+
       $('#func').show();
 
     }
 
   });
 
-  //console.log(interRes);
   $.ajax({
     type: 'POST',
     url: 'http://localhost:8080/delta',
@@ -124,34 +63,6 @@ const sinclick = () => {
 
       drawChart('delta', 'delta-parent', arr, deltaResult, 'delta');
 
-      // let ctx2 = document.getElementById('delta');
-      // let deltaChart = new Chart(ctx2, {
-      //   type: 'line',
-      //   data: {
-      //     labels: arr,
-      //     datasets: [{
-      //       fill: false,
-      //       lineTension: 0.1,
-      //       backgroundColor: "rgba(75,192,192,0.4)",
-      //       borderColor: "rgba(75,192,192,1)",
-      //       borderCapStyle: 'butt',
-      //       borderDash: [],
-      //       borderDashOffset: 0.0,
-      //       borderJoinStyle: 'miter',
-      //       pointBorderColor: "rgba(75,192,192,1)",
-      //       pointBackgroundColor: "#fff",
-      //       pointBorderWidth: 1,
-      //       pointHoverRadius: 5,
-      //       pointHoverBackgroundColor: "rgba(75,192,192,1)",
-      //       pointHoverBorderColor: "rgba(220,220,220,1)",
-      //       pointHoverBorderWidth: 2,
-      //       pointRadius: 1,
-      //       pointHitRadius: 10,
-      //       label: "delta",
-      //       data: deltaResult
-      //     }]
-      //   }
-      // });
       $('#delta').show();
     }
 
@@ -160,6 +71,8 @@ const sinclick = () => {
 };
 
 const sin2click = () => {
+  if(!validation()) return;
+  
   $.ajax({
     type: 'POST',
     url: 'http://localhost:8080/interpolate/sin2',
@@ -258,4 +171,17 @@ const drawChart = (id, parentId, xArr, yArr, label) => {
 const refreshCanvas = (id, parentId) => {
   $('#' + id).remove(); // this is my <canvas> element
   $('#' + parentId).append('<canvas id="' + id + '" width="400" height="400"><canvas>');
+};
+
+const validation = () => {  if($('#startPoint').val() >= $('#endPoint').val()) {
+    $('#startPoint').css({'color' : 'red'});
+    $('#endPoint').css({'color' : 'red'});
+
+    return false;
+  }
+
+  $('#startPoint').css({'color' : 'black'});
+  $('#endPoint').css({'color' : 'black'});
+
+  return true;
 };
