f73a3454abc22ef8c8b5b7ee1f04eb4aca03ee14
ZheniaTrochun <zheniatrochun@ukr.net>
2017-04-15 11:38:06 +0300

    added explanation

--- server/interpolators/newtonInterpolator.js
+++ server/interpolators/newtonInterpolator.js
@@ -6,7 +6,7 @@ const utils = require(__base + 'utils/utils');
 const splinesNum = 100;
 
 class Interpolator {
-  
+
   static interpolate(startPoint, endPoint, funcStr, nodes) {
     const xArrForInterpolation = [];
 
@@ -38,8 +38,14 @@ class Interpolator {
   * n - number of interpolation nodes
   * x - array with x-coordinates of interpolation nodes
   * y - array with y-coordinates of interpolation nodes
+  *
+  * for more info and explanation of interpolation algo, go to https://en.wikipedia.org/wiki/Newton_polynomial
 */
   static NewtonFromStart(item, n, x, y) {
+
+    // res - variable for result
+    // f - temporary variable
+    // den - temporary holder for multiplication results
     let res = 0;
     let f;
     let den;
@@ -62,54 +68,6 @@ class Interpolator {
 
     return res;
   }
-  //
-  // // Newton interpolation
-  // static Newton(x, n, MasX, MasY, step) {
-  //   // creating temporary array
-  //   let mas = [];
-  //   for(let i = 0; i < n + 2; i++) {
-  //     mas[i] = [];
-  //   }
-  //
-  //   for (let i = 0; i < 2; i++) {
-  //     for (let j = 0; j < n + 1; j++) {
-  //       mas[i][j] = i == 0 ? MasX[j] : MasY[j];
-  //     }
-  //   }
-  //
-  //   let m = n;
-  //   for (let i = 2; i < n + 2; i++) {
-  //     for (let j = 0; j < m; j++) {
-  //       mas[i][j] = mas[i - 1][j + 1] - mas[i - 1][j];
-  //     }
-  //     m--;
-  //   }
-  //
-  //   let dy0 = [];
-  //
-  //   for (let i = 1; i <= n + 1; i++) {
-  //     dy0[i-1] = mas[i][0];
-  //   }
-  //
-  //   let res = dy0[0];
-  //   let xn = [];
-  //   xn[0] = x - mas[0][0];
-  //
-  //   for (let i = 1; i < n; i++) {
-  //     let ans = xn[i - 1] * (x - mas[0][i]);
-  //     xn[i] = ans;
-  //     ans = 0;
-  //   }
-  //
-  //   let m1 = n + 1;
-  //   let fact = 1;
-  //   for (let i = 1; i < m1; i++) {
-  //     fact = fact * i;
-  //     res = res + (dy0[i] * xn[i - 1]) / (fact * Math.pow(step, i));
-  //   }
-  //
-  //   return res;
-  // }
 }
 
 module.exports = Interpolator;
